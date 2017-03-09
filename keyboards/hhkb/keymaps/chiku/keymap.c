52325d4e325ad5a6ec27d51d1cb2e270a244ad99
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-03-09 19:59:51 +0900

    Symbols on the number row are put on HHKB layer. 6 and 7 are too far.

--- keyboards/hhkb/keymaps/chiku/keymap.c
+++ keyboards/hhkb/keymaps/chiku/keymap.c
@@ -62,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       | Pwr  | F1  | F2  | F3  | F4 | F5 | F6 | F7 | F8  | F9  | F10 | F11 | F12   | Ins   | Del |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      | CAPS |     |     |     |    |    |    |    | Psc | Slk | Pus | Up  |       | Backs |     |
+      | CAPS |     |     |     |    |    |    |    | Sh 9| Sh0 | Psc | Pus | Slck  | Backs |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      | VoD | VoU | Mut |    |    |Lef |Dow | Up  | Rig | Lef | Rig | Enter |       |     |
+      |      | VoD | VoU | Mut |    |    |Lef |Dow | Up  | Rig | Sh =| Sh 8| Enter |       |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
-      |      |     |     |     |    |    | +  | -  | End | PgD | Dow |     |       |       |     |
+      |      | Sh 1| Sh 2| Sh 3|Sh 4|Sh 5|Sh 6|Sh 7|     |     | Sh -|     |       |       |     |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
 
                  |------+------+----------------------+------+------+
@@ -77,9 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [HHKB] = KEYMAP(
   KC_PWR,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL, \
-  KC_CAPS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_UP  ,  KC_TRNS,  KC_BSPC,          \
-  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP  ,  KC_RGHT,  KC_LEFT,  KC_RGHT,  KC_PENT,                    \
-  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_PPLS,  KC_PMNS,  KC_END,   KC_PGDN,  KC_DOWN,  KC_TRNS,  KC_TRNS,                    \
+  KC_CAPS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_BSPC,          \
+  KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_ASTR,  KC_PLUS,  KC_PENT,                    \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_CIRC,  KC_AMPR,  KC_TRNS,  KC_TRNS,  KC_UNDS,  KC_TRNS,  KC_TRNS,                    \
                      KC_TRNS, KC_TRNS,           KC_TRNS,                KC_TRNS, KC_TRNS)};
 
 
@@ -87,17 +87,39 @@ const uint16_t PROGMEM fn_actions[] = {
 
 };
 
+// This macro is said to be *careless* because it does not care of
+// simultaneous use with other keys. The Left Shift key is kept
+// registered during the key is pressed. There are several phenomenon
+// observed which might be not expected or not predicted.
+// The keycode argument must be 8 bit. See he defintion of register_code
+// and unregister_code.
+#define CARELESS_SHIFT(keycode) case keycode:\
+    if(record->event.pressed) {\
+        register_code(KC_LSFT);\
+        register_code(keycode);\
+    }else {\
+        unregister_code(keycode);\
+        unregister_code(KC_LSFT);\
+    }\
+break;
+
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-  // MACRODOWN only works in this function
       switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
+          CARELESS_SHIFT(KC_1);
+          CARELESS_SHIFT(KC_2);
+          CARELESS_SHIFT(KC_3);
+          CARELESS_SHIFT(KC_4);
+          CARELESS_SHIFT(KC_5);
+          CARELESS_SHIFT(KC_6);
+          CARELESS_SHIFT(KC_7);
+          CARELESS_SHIFT(KC_8);
+          CARELESS_SHIFT(KC_9);
+          CARELESS_SHIFT(KC_0);
+          CARELESS_SHIFT(KC_MINS);
+          CARELESS_SHIFT(KC_EQL);
+          CARELESS_SHIFT(KC_BSLS);
+          CARELESS_SHIFT(KC_GRV);
       }
     return MACRO_NONE;
 };
