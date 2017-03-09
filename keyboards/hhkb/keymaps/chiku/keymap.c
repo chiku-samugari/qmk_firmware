1aad2a74b5eb0f83738b3b556737ef84b1cce6c9
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-03-10 04:25:29 +0900

    SandS enabled.

--- keyboards/hhkb/keymaps/chiku/keymap.c
+++ keyboards/hhkb/keymaps/chiku/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,          \
   KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
   KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_FN0,                      \
-                    MO(HHKB),  KC_LALT,  /*        */ KC_SPC,      KC_GRV,   KC_RGUI),
+                    MO(HHKB),  KC_LALT, MT(MOD_LSFT, KC_SPACE),    KC_GRV,   KC_RGUI),
 
 
     /* Layer Arrow : Arrow mode. Both HJKL and HHKB original diamond
