d0dd918bb485607fcab3d31d7596ba60c5ad6670
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-03-10 00:08:30 +0900

    TT is used instead of TG. I finally found out TAPPING_TOGGLE!

---
+++ keyboards/hhkb/keymaps/chiku/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_CHIKU_H
+#define CONFIG_CHIKU_H
+
+#include "../../config.h"
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
+#endif
