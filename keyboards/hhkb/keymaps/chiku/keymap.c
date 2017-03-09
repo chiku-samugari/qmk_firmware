a22dbe6e05f5661352ae2fa8d3fb75905b0c0059
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-03-10 01:23:54 +0900

    A vairation of TT that utilize different layer between hold and tap.

--- keyboards/hhkb/keymaps/chiku/keymap.c
+++ keyboards/hhkb/keymaps/chiku/keymap.c
@@ -2,6 +2,7 @@
  * default HHKB Layout
  */
 #include "hhkb.h"
+#include "config.h"
 
 #define BASE 0
 #define ARROW 1
@@ -10,6 +11,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* BASE Level: Default Layer
+     * Fn0: Activates HHKB Layer on hold, toggle ARROW layer on
+     *      TAPPING_TOGGLE times tap.
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | BS    |BS |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
@@ -17,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Cont  | A | S | D | F | G | H | J | K | L | ; | '     | Ent |       |   |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Shift | Z | X | C | V | B | N | M | , | . | / | Shift | Fn1 |       |   |
+     | Shift | Z | X | C | V | B | N | M | , | . | / | Shift | Fn0 |       |   |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
 
             |------+------+-----------------------+------+------|
@@ -29,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSPC, KC_BSPC, \
   KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,          \
   KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
-  KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  TT(ARROW),                   \
+  KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_FN0,                      \
                     MO(HHKB),  KC_LALT,  /*        */ KC_SPC,      KC_GRV,   KC_RGUI),
 
 
@@ -84,8 +87,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 const uint16_t PROGMEM fn_actions[] = {
-
+    [0] = ACTION_FUNCTION_TAP(0),
 };
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    switch(id) {
+        case 0:
+            if(record->event.pressed) {
+                layer_on(HHKB);
+            }else {
+                layer_off(HHKB);
+                if(record->tap.count == TAPPING_TOGGLE) {
+                    layer_invert(ARROW);
+                }
+            }
+            break;
+    }
+}
 
 // This macro is said to be *careless* because it does not care of
 // simultaneous use with other keys. The Left Shift key is kept
