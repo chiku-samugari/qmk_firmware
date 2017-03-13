a5e41e21b951299582dece3e05209a2eff113c12
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-03-13 19:56:11 +0900

    A layer pretend as an US layout when it is recognized as a JIS keyboard.

--- keyboards/hhkb/keymaps/chiku/keymap.c
+++ keyboards/hhkb/keymaps/chiku/keymap.c
@@ -5,13 +5,17 @@
 #include "config.h"
 
 #define BASE 0
-#define ARROW 1
-#define HHKB 2
+#define US_ON_JIS 1
+#define SHIFT_US_ON_JIS 2
+#define ARROW 3
+#define HHKB 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* BASE Level: Default Layer
-     * Fn0: Activates HHKB Layer on hold, toggle ARROW layer on
+     * Fn0: Activates HHKB Layer on hold, toggles ARROW layer on
+     *      TAPPING_TOGGLE times tap.
+     * Fn1: Activates HHKB Layer on hold, toggles US_ON_JIS layer on
      *      TAPPING_TOGGLE times tap.
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | BS    |BS |
@@ -20,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Cont  | A | S | D | F | G | H | J | K | L | ; | '     | Ent |       |   |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
-     | Shift | Z | X | C | V | B | N | M | , | . | / | Shift | Fn0 |       |   |
+     | Shift | Z | X | C | V | B | N | M | , | . | / | Shift | Fn1 |       |   |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
 
             |------+------+-----------------------+------+------|
@@ -32,8 +36,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,     KC_9,    KC_0,     KC_MINS,  KC_EQL,    KC_BSPC, KC_BSPC, \
   KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,          \
   KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
-  KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_FN0,                      \
+  KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_FN1,                      \
                     KC_FN0,  KC_LALT,    /*        */ KC_SPC,      KC_GRV,   KC_RGUI),
+
+
+    /* Layer US_ON_JIS : The layer behaves as an US keyboard when the
+     * keyboard is recognized as a JIS keyboard.
+     * Fn2: register/unregister shift modifier and on/off
+     *      SHIFT_US_ON_JIS layer.
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |     |    |    |    |    |     |     |     |     | S(-)  | YEN   |S([) |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |     |    |    |    |    |     |     |     | ]   | \     | BS    |     |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |     |    |    |    |    |     |     |     | S(7)|       |       |     |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      | Fn2  |     |     |     |    |    |    |    |     |     |     |     |       |       |     |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+
+                 |------+------+----------------------+------+------+
+                 | **** | **** | ******************** | **** | **** |
+                 |------+------+----------------------+------+------+
+
+     */
+
+  [US_ON_JIS] = KEYMAP(
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_MINS), KC_JYEN, S(KC_LBRC), \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RBRC, KC_BSLS, KC_BSPC,          \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_7), KC_TRNS,                   \
+  KC_FN2,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   \
+                     KC_TRNS, KC_TRNS,           KC_TRNS,                KC_TRNS, KC_TRNS),
+
+
+    /* Layer SHIFT_US_ON_JIS : The layer behaves as an US keyboard
+     * modified by the left shift when the keyboard is recognized as a
+     * JIS keyboard.
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     | [   |     |    |    | =  |S(6)| S(')| S(8)| S(9)|S(RO)| S(;)  |       |S(=) |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |     |    |    |    |    |     |     |     |     |       |       |     |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |     |    |    |    |    |     |     | '   | S(2)|       |       |     |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+      |      |     |     |     |    |    |    |    |     |     |     |     |       |       |     |
+      |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
+
+                 |------+------+----------------------+------+------+
+                 | **** | **** | ******************** | **** | **** |
+                 |------+------+----------------------+------+------+
+
+     */
+
+  [SHIFT_US_ON_JIS] = KEYMAP( \
+  KC_TRNS, KC_TRNS, KC_FN3, KC_TRNS, KC_TRNS, KC_TRNS, KC_FN3,  S(KC_6), S(KC_QUOT), S(KC_8), S(KC_9), S(KC_RO), S(KC_SCLN), KC_TRNS, S(KC_EQL), \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,          \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_FN3, S(KC_2), KC_TRNS,                   \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   \
+                     KC_TRNS, KC_TRNS,           KC_TRNS,                KC_TRNS, KC_TRNS),
 
 
     /* Layer Arrow : Arrow mode. Both HJKL and HHKB original diamond
@@ -88,7 +147,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_FUNCTION_TAP(0),
+    [1] = ACTION_FUNCTION_TAP(1),
+    [2] = ACTION_FUNCTION(2),
+    [3] = ACTION_FUNCTION(3),
 };
+
+static uint8_t unshift_count = 0;
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
     switch(id) {
@@ -99,6 +163,59 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
                 layer_off(HHKB);
                 if(record->tap.count == TAPPING_TOGGLE) {
                     layer_invert(ARROW);
+                }
+            }
+            break;
+        case 1:
+            if(record->event.pressed) {
+                layer_on(HHKB);
+            }else {
+                layer_off(HHKB);
+                if(record->tap.count == TAPPING_TOGGLE) {
+                    layer_invert(US_ON_JIS);
+                }
+            }
+            break;
+        case 2:
+            unshift_count = 0;
+            if(record->event.pressed) {
+                register_mods(MOD_LSFT);
+                layer_on(SHIFT_US_ON_JIS);
+            }else {
+                unregister_mods(MOD_LSFT);
+                layer_off(SHIFT_US_ON_JIS);
+            }
+            break;
+        case 3:
+            if(record->event.pressed) {
+                unregister_mods(MOD_LSFT);
+                unshift_count += 1;
+                switch(keymap_key_to_keycode(BASE, record->event.key)) {
+                    case KC_2:      // @
+                        register_code(KC_LBRC);
+                        break;
+                    case KC_6:      // ^
+                        register_code(KC_EQL);
+                        break;
+                    case KC_SCLN:   // :
+                        register_code(KC_QUOT);
+                        break;
+                }
+            } else {
+                unshift_count -= 1;
+                switch(keymap_key_to_keycode(BASE, record->event.key)) {
+                    case KC_2:
+                        unregister_code(KC_LBRC);
+                        break;
+                    case KC_6:
+                        unregister_code(KC_EQL);
+                        break;
+                    case KC_SCLN:
+                        unregister_code(KC_QUOT);
+                        break;
+                }
+                if(unshift_count == 0) {
+                   register_mods(MOD_LSFT);
                 }
             }
             break;
