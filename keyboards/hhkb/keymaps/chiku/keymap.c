31da87912d74d686bd1525051cc122add281eb54
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-03-13 19:18:00 +0900

    Left Fn is changed to layer tap toggle key. HHKB on hold, ARROW on tap.

--- keyboards/hhkb/keymaps/chiku/keymap.c
+++ keyboards/hhkb/keymaps/chiku/keymap.c
@@ -13,7 +13,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* BASE Level: Default Layer
      * Fn0: Activates HHKB Layer on hold, toggle ARROW layer on
      *      TAPPING_TOGGLE times tap.
-     * MO: Activates HHKB Layer on hold.
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | BS    |BS |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
@@ -25,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
 
             |------+------+-----------------------+------+------|
-            | MO   | LAlt | ******* Space ******* | `    | RGUI |
+            | Fn0  | LAlt | ******* Space ******* | `    | RGUI |
             |------+------+-----------------------+------+------|
     */
 
@@ -34,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,     KC_O,    KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,          \
   KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,     KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,                      \
   KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,  KC_FN0,                      \
-                    MO(HHKB),  KC_LALT,  /*        */ KC_SPC,      KC_GRV,   KC_RGUI),
+                    KC_FN0,  KC_LALT,    /*        */ KC_SPC,      KC_GRV,   KC_RGUI),
 
 
     /* Layer Arrow : Arrow mode. Both HJKL and HHKB original diamond
