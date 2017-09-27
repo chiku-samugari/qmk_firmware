0dedeae6ab364a35b20048a43966b8e30f1c0235
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-09-28 01:51:14 +0900

    SELECTOR layer is introduced to pick a layer.

--- keyboards/hhkb/keymaps/chiku/keymap.c
+++ keyboards/hhkb/keymaps/chiku/keymap.c
@@ -7,6 +7,7 @@
 
 enum layer_ids {
     BASE = 0,
+    SELECTOR,
     MOD_US,
     MOD_US_ON_JIS,
     SHIFT_MOD_US_ON_JIS,
@@ -23,8 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer BASE: Default Layer
      * Fn0: Activates SYMB_AND_FN Layer on hold, toggles ARROW layer on
      *      TAPPING_TOGGLE times tap.
-     * Fn1: Activates SYMB_AND_FN Layer on hold, toggles US_ON_JIS layer
-     *      on TAPPING_TOGGLE times tap.
+     * Fn1: Activates SYMB_AND_FN Layer on hold, switches on SELECTOR
+     *      layer on TAPPING_TOGGLE times tap. Beforehand, layer_state
+     *      is cleared on first tap.
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
      | Esc   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | -     | =   | BS    |BS |
      |-------+---+---+---+---+---+---+---+---+---+---+-------+-----+-------+---|
@@ -47,6 +49,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LSFT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_BSLS,  KC_FN1,                      \
                     KC_FN0,  KC_LALT,      KC_SPC,      KC_GRV,   KC_RGUI),
 
+  /* Layer SELECTOR: pick a layer that is thought as an independent
+   *                 keyboard such as US_ON_JIS or MOUSE.
+   */
+  [SELECTOR] = KEYMAP(
+  KC_TRNS, KC_FN6, KC_FN7, KC_FN8, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_FN9, KC_FN10, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               \
+                    KC_TRNS,  KC_TRNS,      KC_TRNS,      KC_TRNS,   KC_TRNS),
+
+
     /* Layer MOD_US: LShift + Semicolon issues KC_QUOTE, Quote issues
      *               LShift and Semicolon. My favorite setup and I
      *               normally achieve it by xmodmap.
@@ -65,8 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                   this layer is not for me, the layout is HHKB
      *                   Pro 2 original layout with DIP 3,4, and 5 are
      *                   on.
-     * Fn2: register/unregister shift modifier and on/off
-     *      SHIFT_US_ON_JIS layer.
+     * Fn2: register/unregister left shift modifier and on/off
+     *      SHIFT_US_ON_JIS layer. Please be aware that the Right Shift
+     *      also works as a Left Shift key.
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
       |      |     |     |     |    |    |    |    |     |     |     |     | S(-)  | YEN   |S([) |
       |------+-----+-----+-----+----+----+----+----+-----+-----+-----+-----+-------+-------+-----|
@@ -87,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_MINS), KC_JYEN, S(KC_LBRC), \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RBRC, KC_BSLS, KC_BSPC,          \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_7), KC_TRNS,                   \
-  KC_FN2,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   \
+  KC_FN2,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_FN2,  KC_TRNS,                   \
                      KC_TRNS, KC_TRNS,           KC_TRNS,                KC_RALT, KC_RGUI),
 
 
@@ -111,11 +125,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
   [SHIFT_US_ON_JIS] = KEYMAP( \
-  KC_TRNS, S(KC_1), KC_FN3, S(KC_3), S(KC_4), S(KC_5), KC_FN3,  S(KC_6), S(KC_QUOT), S(KC_8), S(KC_9), S(KC_RO), S(KC_SCLN), S(KC_JYEN), S(KC_EQL), \
-  KC_TRNS, S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T), S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P), S(KC_RBRC), S(KC_BSLS), KC_TRNS,          \
-  KC_TRNS, S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G), S(KC_H), S(KC_J), S(KC_K), S(KC_L), KC_FN3, S(KC_2), KC_TRNS,                   \
-  KC_TRNS, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT), S(KC_SLSH), KC_TRNS, KC_TRNS,                   \
-                     KC_TRNS, KC_TRNS,           KC_TRNS,                KC_TRNS, KC_TRNS),
+  S(KC_ESC), S(KC_1), KC_FN3, S(KC_3), S(KC_4), S(KC_5), KC_FN3,  S(KC_6), S(KC_QUOT), S(KC_8), S(KC_9), S(KC_RO), S(KC_SCLN), S(KC_JYEN), S(KC_EQL), \
+  S(KC_TAB), S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T), S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P), S(KC_RBRC), S(KC_BSLS), S(KC_BSPC),            \
+  S(KC_LCTL),S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G), S(KC_H), S(KC_J), S(KC_K), S(KC_L), KC_FN3, S(KC_2), S(KC_ENT),                             \
+  KC_TRNS, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_RSFT), KC_TRNS,                     \
+                     KC_TRNS, S(KC_LALT),           S(KC_SPC),                S(KC_RALT), S(KC_RGUI)),
+
+  /* Layer MOD_US_ON_JIS: The layer behaves as a right pinky fixed US
+   *                      keyboard when the keyboard is recognized as a
+   *                      JIS keyboard. Since this layer is for me, the
+   *                      position of Backslash key, Grave key,
+   *                      Backspace key are changed and right Alt key
+   *                      disappeared.
+   */
+  [MOD_US_ON_JIS] = KEYMAP(
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_MINS), KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_RBRC, KC_BSLS, KC_BSPC,          \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   \
+  KC_FN5,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_JYEN, KC_TRNS,                   \
+                     KC_TRNS, KC_TRNS,           KC_TRNS,                S(KC_LBRC), KC_TRNS),
+
+  [SHIFT_MOD_US_ON_JIS] = KEYMAP( \
+  S(KC_ESC), S(KC_1), KC_FN3, S(KC_3), S(KC_4), S(KC_5), KC_FN3,  S(KC_6), S(KC_QUOT), S(KC_8), S(KC_9), S(KC_RO), S(KC_SCLN), S(KC_BSPC), S(KC_BSPC), \
+  S(KC_TAB), S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T), S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P), S(KC_RBRC), S(KC_BSLS), S(KC_BSPC),             \
+  S(KC_LCTL),S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G), S(KC_H), S(KC_J), S(KC_K), S(KC_L), S(KC_7), S(KC_2), S(KC_ENT),                             \
+  KC_TRNS, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B), S(KC_N), S(KC_M), S(KC_COMM), S(KC_DOT), S(KC_SLSH), S(KC_JYEN), KC_TRNS,                      \
+                     KC_TRNS, S(KC_LALT),           S(KC_SPC),                S(KC_EQL), S(KC_RGUI)),
 
 
     /* Layer Arrow: Arrow mode. Both HJKL and HHKB original diamond
@@ -173,7 +208,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_PWR,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,   KC_DEL, \
   KC_CAPS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS, TG(MOUSE), KC_TRNS,  KC_TRNS,  KC_LPRN,  KC_RPRN,  KC_PSCR,  KC_SLCK,  KC_PAUS,  KC_BSPC,          \
   KC_TRNS,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_ASTR,  KC_PLUS,  KC_PENT,                    \
-  KC_TRNS,TG(MOD_US), KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_CIRC,  KC_AMPR,  KC_TRNS,  KC_TRNS,  KC_UNDS,  KC_TRNS,  KC_TRNS,                    \
+  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_CIRC,  KC_AMPR,  KC_TRNS,  KC_TRNS,  KC_UNDS,  KC_TRNS,  KC_TRNS,                    \
                      KC_TRNS, KC_TRNS,           KC_TRNS,                KC_TRNS, KC_TRNS),
 
 
@@ -231,19 +266,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 enum fn_action_ids {
     TT_HHKB_ARROW = 0,
-    TT_HHKB_US_ON_JIS,
+    TT_HHKB_SELECTOR,
     MOMENTARY_LAYER_AND_SHIFT,
     UNSHIFT,
     FIXED_RIGHT_PINKY,
+    SELECT_KEYBOARD,
 };
 
 const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_FUNCTION_TAP(TT_HHKB_ARROW),
-    [1] = ACTION_FUNCTION_TAP(TT_HHKB_US_ON_JIS),
+    [1] = ACTION_FUNCTION_TAP(TT_HHKB_SELECTOR),
     [2] = ACTION_FUNCTION_OPT(MOMENTARY_LAYER_AND_SHIFT, SHIFT_US_ON_JIS),
     [3] = ACTION_FUNCTION(UNSHIFT),
     [4] = ACTION_FUNCTION(FIXED_RIGHT_PINKY),
     [5] = ACTION_FUNCTION_OPT(MOMENTARY_LAYER_AND_SHIFT, SHIFT_MOD_US_ON_JIS),
+    [6] = ACTION_FUNCTION_OPT(SELECT_KEYBOARD, MOD_US),
+    [7] = ACTION_FUNCTION_OPT(SELECT_KEYBOARD, ARROW),
+    [8] = ACTION_FUNCTION_OPT(SELECT_KEYBOARD, MOUSE),
+    [9] = ACTION_FUNCTION_OPT(SELECT_KEYBOARD, US_ON_JIS),
+    [10] = ACTION_FUNCTION_OPT(SELECT_KEYBOARD, MOD_US_ON_JIS),
 };
 
 typedef uint8_t Layer;
@@ -326,8 +367,26 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt) {
         case TT_HHKB_ARROW:
             layer_tap_toggle_ab(record, SYMB_AND_FN, ARROW);
             break;
-        case TT_HHKB_US_ON_JIS:
-            layer_tap_toggle_ab(record, SYMB_AND_FN, US_ON_JIS);
+        case TT_HHKB_SELECTOR:
+            if(record->event.pressed) {
+                layer_on(SYMB_AND_FN);
+            }else {
+                layer_off(SYMB_AND_FN);
+                if(record->tap.count == 1) {
+                    layer_clear();
+                }
+                // This `if` statement should be an independent one
+                // because TAPPING_TOGGLE can be 1.
+                if(record->tap.count == TAPPING_TOGGLE) {
+                    layer_on(SELECTOR);
+                }
+            }
+            break;
+        case SELECT_KEYBOARD:
+            if(record->event.pressed) {
+                layer_off(SELECTOR);
+                layer_on(opt);
+            }
             break;
         case MOMENTARY_LAYER_AND_SHIFT:
             unshift_count = 0;
