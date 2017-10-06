2d72c92c022141cf2178fdf39987530e60042ff5
chiku <samugari@is.s.u-tokyo.ac.jp>
2017-10-07 01:03:21 +0900

    The lowest row is rearranged.

--- keyboards/hhkb/keymaps/chiku_jp/keymap.c
+++ keyboards/hhkb/keymaps/chiku_jp/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Shft   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|  \| Up|Sft|
      * |-----------------------------------------------------------|
-     * |   ||Fn0|Alt|Cmd|   |   Spc   |Bsp|   |   |   ||Lft|Dwn|Rgh|
+     * |   ||Fn0|GUI|Alt| = |   Spc   | ` |ALT|   |Fn0||Lft|Dwn|Rgh|
      * `-----------------------------------------------------------'
      */
 
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH,   KC_RO,   KC_UP, KC_RSFT,
-    _______,KC_FN0, KC_LGUI, KC_LALT, KC_MHEN,         KC_SPC,    KC_HENK,  KC_KANA, KC_RALT,  KC_FN0, KC_LEFT, KC_DOWN, KC_RGHT
+    _______,KC_FN0, KC_LGUI, KC_LALT,  KC_EQL,         KC_SPC,     KC_GRV,  KC_RALT, _______,  KC_FN0, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* Layer Auxiliary: Extended version of HHKB Fn layer.
