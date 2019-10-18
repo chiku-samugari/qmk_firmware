146d3ef2391fc71cdd4441ba03ae081c522bdcea
chiku <samugari@is.s.u-tokyo.ac.jp>
2019-10-18 17:14:19 +0900

    Left Alt and MO(SHIFT_NUM_AND_SYM) are swapped on my KUMO keyboard.

--- keyboards/thevankeyboards/kumo/keymaps/chiku/keymap.c
+++ keyboards/thevankeyboards/kumo/keymaps/chiku/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(NUM_AND_SYM, KC_QUOTE),
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    MO(MOUSE), MO(AUXILIARY), MO(SHIFT_NUM_AND_SYM), KC_LALT,    KC_SPC,  KC_GRV,    KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT
+    MO(MOUSE), MO(AUXILIARY), KC_LALT, MO(SHIFT_NUM_AND_SYM),    KC_SPC,  KC_GRV,    KC_EQL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [NUM_AND_SYM] = LAYOUT_arrow_command(
