cbe38abee1dec840a2c69e56623a885f06182799
chiku <samugari@is.s.u-tokyo.ac.jp>
2019-11-05 01:24:59 +0900

    KUMO's Number layer key is moved to right next of the space key.

--- keyboards/thevankeyboards/kumo/keymaps/chiku/keymap.c
+++ keyboards/thevankeyboards/kumo/keymaps/chiku/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_arrow_command(
      KC_ESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(NUM_AND_SYM, KC_QUOTE),
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    MO(MOUSE), MO(AUXILIARY), KC_FN0, MO(SHIFT_NUM_AND_SYM),    KC_SPC,  KC_GRV,    KC_EQL, KC_BSPC, KC_0, KC_ENT
+    MO(MOUSE), MO(AUXILIARY), KC_FN0, MO(SHIFT_NUM_AND_SYM),    KC_SPC,  LT(NUM_AND_SYM, KC_GRAVE),    KC_EQL, KC_BSPC, KC_0, KC_ENT
   ),
 
   /* In oreder to use Alt+Tab */
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB, _______,  KC_GRV,  KC_EQL, KC_MINS, _______, _______, _______, _______, _______, _______,  KC_DEL,
     _______, KC_BSLS, KC_QUOT, KC_LBRC, KC_RBRC,  KC_GRV,  KC_EQL, KC_SCLN,    KC_9,    KC_0, KC_MINS, _______,
     _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8, _______, _______, _______,
-    _______, _______, _______, _______,          _______,  KC_EQL,          _______, _______, _______, _______
+    _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
   ),
 
   [SHIFT_NUM_AND_SYM] = LAYOUT_arrow_command(
