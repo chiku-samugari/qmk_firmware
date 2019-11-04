f6df0870d95264a892305d8e9e6943a5287242c1
chiku <samugari@is.s.u-tokyo.ac.jp>
2019-11-05 02:59:39 +0900

    chiku keymap for UT47.2

---
+++ keyboards/ut472/keymaps/chiku/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "config_common.h"
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
+#undef PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
