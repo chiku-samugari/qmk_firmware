f9c88e251a02764e223bf64939d547b0b0c2f2a0
chiku <samugari@is.s.u-tokyo.ac.jp>
2021-07-20 23:14:16 +0900

    [HHKB] Right bottom arrows arrangement is changed to Vim HJKL style.

--- keyboards/hhkb/keymaps/chiku_jp/keymap.c
+++ keyboards/hhkb/keymaps/chiku_jp/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------------------------------------------------------------------------|
      * |L-Shift |  Z |  X |  C |  V |  B |  N |  M |  , |  . |  / |  \  | Up |RSft|
      * |--------------------------------------------------------------------------|
-     * | Fn0| | Fn1|LGUI|Fn12|LAlt|  Space   | `  |  = |RAlt| Fn2| |Left|Down|Rght|
+     * | Fn0| | Fn1|LGUI|Fn12|LAlt|  Space   | `  |  = | Fn2|Left| |Down| Up |Rght|
      * `--------------------------------------------------------------------------'
      */
 
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,     KC_K,    KC_L, KC_SCLN, KC_QUOT, KC_ENT,  KC_ENT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_BSLS,   KC_UP, KC_RSFT,
-    KC_FN0, KC_FN1, KC_LGUI, KC_FN12, KC_LALT,         KC_SPC,     KC_GRV, KC_EQL, KC_RALT,  KC_FN2, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_FN0, KC_FN1, KC_LGUI, KC_FN12, KC_LALT,         KC_SPC,     KC_GRV, KC_EQL,    KC_FN2, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
   ),
 
   /* Layer SELECTOR:
